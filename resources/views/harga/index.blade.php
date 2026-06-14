6ab9d74d2125924cd14d6afaa5078dbb85647bba
SerlinYolanda <serlinyolanda05@gmail.com>
2026-06-14 16:53:12 +0700

    perubahan 1

--- resources/views/harga/index.blade.php
+++ resources/views/harga/index.blade.php
@@ -25,7 +25,7 @@
                             Kategori
                         </th>
                         <th scope="col" class="px-6 py-3 text-center">
-                            Satuan
+                            Satuansssssss
                         </th>
                         <th scope="col" class="px-6 py-3 text-right">
                             Harga
